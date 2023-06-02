1efab8186bb9979ed08021fa302807c260defce0
krystianklimczak <krystianklimczak1996r@gmail.com>
2023-06-02 15:03:21 +0200

    fx modal-contact

--- src/js/modal-contact.js
+++ src/js/modal-contact.js
@@ -4,7 +4,7 @@
     closeModalBtn: document.querySelector('[data-modal-contact-close]'),
     modal: document.querySelector('[data-modal-contact]'),
   };
-  for (let i = 0; i < openModalBtns.length; i++) {
+  for (let i = 0; i < refs.openModalBtns.length; i++) {
     refs.openModalBtns[i].addEventListener('click', toggleModal);
   }
   refs.closeModalBtn.addEventListener('click', toggleModal);
