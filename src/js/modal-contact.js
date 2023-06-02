5f773d8e25c3153e68cc7fd5edf70eafc43d4911
krystianklimczak <krystianklimczak1996r@gmail.com>
2023-06-02 14:55:37 +0200

    change script for modal-contact

--- src/js/modal-contact.js
+++ src/js/modal-contact.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-contact-open]"),
-    closeModalBtn: document.querySelector("[data-modal-contact-close]"),
-    modal: document.querySelector("[data-modal-contact]"),
+    openModalBtns: document.querySelectorAll('[data-modal-contact-open]'),
+    closeModalBtn: document.querySelector('[data-modal-contact-close]'),
+    modal: document.querySelector('[data-modal-contact]'),
   };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
+  for (let i = 0; i < openModalBtns.length; i++) {
+    refs.openModalBtns[i].addEventListener('click', toggleModal);
+  }
+  refs.closeModalBtn.addEventListener('click', toggleModal);
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle('is-hidden');
   }
 })();
